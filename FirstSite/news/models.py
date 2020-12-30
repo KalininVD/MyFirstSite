b5e8d2f0de57d08182960280e3fa66f7f33c0800
VLAD <kalinin.vd@yandex.ru>
2020-12-30 14:24:45 +0300

    Added news about Google and Apple

--- FirstSite/news/models.py
+++ FirstSite/news/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Articles(models.Model):
     title = models.CharField('Название статьи', max_length=50)
+    company = models.CharField('Название компании', max_length=20, default='')
     anons = models.CharField('Анонс', max_length=250)
     full_text = models.TextField('Статья')
     date = models.DateTimeField('Дата публикации')
